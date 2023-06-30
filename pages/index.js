be0be62276e454cd4f1712cf5ac820fa74b5b265
abel muro <abelmuro93@gmail.com>
2023-06-30 10:28:17 -0700

    commit

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import CreateTodo from '../components/NewTodo/CreateTodo';
 import ShowTodos from '../components/DisplayTodos/ShowTodos';
 import AppLayout from '../components/AppLayout';
 import DragDropMessage from '../components/DragDropMessage';
+import PreLoadImages from '../components/PreLoadImages';
 import { DndProvider } from "react-dnd"
 import { HTML5Backend } from "react-dnd-html5-backend"
 
